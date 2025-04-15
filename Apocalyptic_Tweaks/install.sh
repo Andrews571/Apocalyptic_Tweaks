01c60f1f3ebc3d2dc0e629cc68e2b98b8864960c
Andrews571 <97290390+Andrews571@users.noreply.github.com>
2025-04-15 10:32:02 -0300

    Update install.sh

--- Apocalyptic_Tweaks/install.sh
+++ Apocalyptic_Tweaks/install.sh
@@ -23,6 +23,6 @@ on_install() {
 
 set_permissions() {
   set_perm_recursive "$MODPATH" 0 0 0755 0644
-  set_perm_recursive "$MODPATH/ABattery" 0 0 0774 0774
+  set_perm_recursive "$MODPATH/ABattery/ABatteryReset.sh" 0 0 0775 0775
   set_perm "$MODPATH/service.sh" 0 0 0755
 }
