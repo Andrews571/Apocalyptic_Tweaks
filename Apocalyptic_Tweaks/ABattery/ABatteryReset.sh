b9b662a5c22b33fcb07ba32ab658c0e20c85c353
Rip-Phoenix <ripphoenixawk@gmail.com>
2025-04-15 10:00:04 -0300

    Update ABatteryReset.sh
    optimize logic.

--- Apocalyptic_Tweaks/ABattery/ABatteryReset.sh
+++ Apocalyptic_Tweaks/ABattery/ABatteryReset.sh
@@ -1,31 +1,33 @@
-#!/bin/bash
+#!/system/bin/sh
 
-# Battery Modifications
-resetprop -n persist.sys.shutdown.mode hibernate
-resetprop -n persist.radio.add_power_save 1
-resetprop -n wifi.supplicant_scan_interval 300 
-resetprop -n ro.ril.disable.power.collapse 1
-resetprop -n ro.config.hw_fast_dormancy 1
-resetprop -n ro.semc.enable.fast_dormancy true
-resetprop -n ro.config.hw_quickpoweron true
-resetprop -n ro.mot.eri.losalert.delay 1000
-resetprop -n ro.config.hw_power_saving true
-resetprop -n pm.sleep_mode 1
-resetprop -n ro.ril.sensor.sleep.control 1
-resetprop -n power_supply.wakeup enable
+# Modificações de bateria
+props=(
+  "persist.sys.shutdown.mode=hibernate"
+  "persist.radio.add_power_save=1"
+  "wifi.supplicant_scan_interval=300"
+  "ro.ril.disable.power.collapse=1"
+  "ro.config.hw_fast_dormancy=1"
+  "ro.semc.enable.fast_dormancy=true"
+  "ro.config.hw_quickpoweron=true"
+  "ro.mot.eri.losalert.delay=1000"
+  "ro.config.hw_power_saving=true"
+  "pm.sleep_mode=1"
+  "ro.ril.sensor.sleep.control=1"
+  "power_supply.wakeup=enable"
+  "ro.ril.power.collapse=1"
+  "power.saving.enabled=1"
+  "battery.saver.low_level=30"
+  "power.saving.enable=1"
+  "persist.radio.apm_sim_not_pwdn=1"
+  "ro.ril.enable.amr.wideband=0"
+  "power.saving.low_screen_brightness=1"
+  "ro.config.hw_smart_battery=1"
+  "ro.setupwizard.mode=DISABLED"
+  "persist.sys.gmaps_hack=1"
+)
 
-# Additional Battery Optimizations
-resetprop -n ro.ril.power.collapse 1
-resetprop -n power.saving.enabled 1
-resetprop -n battery.saver.low_level 30
-resetprop -n power.saving.enable 1
-resetprop -n persist.radio.apm_sim_not_pwdn 1
-resetprop -n ro.ril.enable.amr.wideband 0
-resetprop -n power.saving.low_screen_brightness 1
-resetprop -n ro.config.hw_smart_battery 1
-
-# Dalvik and Kernel Modifications
-resetprop -n ro.setupwizard.mode DISABLED
-
-# Miscellaneous
-resetprop -n persist.sys.gmaps_hack 1
+for prop in "${props[@]}"; do
+  key="${prop%%=*}"
+  value="${prop#*=}"
+  resetprop -n "$key" "$value"
+done
